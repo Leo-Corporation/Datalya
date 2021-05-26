e0aa321ececfa4939262f41a2b886e9b15ff8a4b
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-05-26 11:19:48 +0200

    Datagrid columns set to auto-generate

--- Datalya/Pages/DatabasePage.xaml.cs
+++ Datalya/Pages/DatabasePage.xaml.cs
@@ -21,6 +21,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. 
 */
+using Datalya.Classes;
+using Datalya.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
