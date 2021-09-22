2fed8faa39145b04fcda8183b5c4ff468c16b400
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-09-22 13:19:42 +0200

    Version 1.2.0.2109-pre1

--- Datalya/Classes/Global.cs
+++ Datalya/Classes/Global.cs
@@ -46,7 +46,7 @@ namespace Datalya.Classes
 		/// <summary>
 		/// Datalya's version.
 		/// </summary>
-		public static string Version => "1.1.0.2108";
+		public static string Version => "1.2.0.2109-pre1";
 
 		/// <summary>
 		/// Last version of Datalya.
